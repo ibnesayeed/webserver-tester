6b944ee33946ee5f7d7f60199be4397dcd64a4d4
Sawood Alam <ibnesayeed@gmail.com>
2018-10-02 20:18:06 -0400

    Load test cases in a subsequent request

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ def jsonify_result(result):
 
 @app.route("/")
 def home():
-    return render_template("index.html", test_buckets=bucket_numbers, test_cases=test_cases, student_ids=student_repos.keys(), show_deployer=DEPLOYER)
+    return render_template("index.html", test_buckets=bucket_numbers, student_ids=student_repos.keys(), show_deployer=DEPLOYER)
 
 
 @app.route("/servers/<csid>")
