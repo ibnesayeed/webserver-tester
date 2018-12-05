a70ed243b43d9c2153b7faaf013cb2ed3c6a3bca
Sawood Alam <ibnesayeed@gmail.com>
2018-12-04 22:13:49 -0500

    Update default message template reference

--- servertester/testsuites/cs531a4.py
+++ servertester/testsuites/cs531a4.py
@@ -4,119 +4,119 @@ from ..base.httptester import HTTPTester
 class CS531A4(HTTPTester):
     """CS531A4 is a special purpose HTTPTester with test cases for Assignment 4 of the CS531 (Web Server Design) course"""
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_1(self, report):
         """Test case 1"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_2(self, report):
         """Test case 2"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 200)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_3(self, report):
         """Test case 3"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_4(self, report):
         """Test case 4"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 200)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_5(self, report):
         """Test case 5"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_6(self, report):
         """Test case 6"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_7(self, report):
         """Test case 7"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 200)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_8(self, report):
         """Test case 8"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_9(self, report):
         """Test case 9"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_10(self, report):
         """Test case 10"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 200)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_11(self, report):
         """Test case 11"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_12(self, report):
         """Test case 12"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_13(self, report):
         """Test case 13"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 400)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_14(self, report):
         """Test case 14"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_15(self, report):
         """Test case 15"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 401)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_16(self, report):
         """Test case 16"""
         assert False, "Yet to be implemented!"
         self.check_status_is(report, 416)
 
 
-    @HTTPTester.request("get-root.http")
+    @HTTPTester.request("get-url-ua.http", PATH="/a4-test/", USERAGENT="CS 531-f18 A4 automated Checker")
     def test_17(self, report):
         """Test case 17"""
         assert False, "Yet to be implemented!"
