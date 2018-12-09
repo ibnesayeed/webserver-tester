f668342c74fcde480dc87276507ca5e53aea00cf
Sawood Alam <ibnesayeed@gmail.com>
2018-12-08 22:16:44 -0500

    Add boilerplate assignment 5 test suite

---
+++ servertester/testsuites/cs531a5.py
@@ -0,0 +1,123 @@
+import hashlib
+
+from ..base.httptester import HTTPTester
+
+
+class CS531A5(HTTPTester):
+    """CS531A5 is a special purpose HTTPTester with test cases for Assignment 5 of the CS531 (Web Server Design) course"""
+
+    # A helper method to be used in testing Digest authentication
+    def generate_digest_values(self, nonce):
+        cnonce = hashlib.md5(b"go hokies").hexdigest()
+        ncount1 = "00000001"
+        ncount2 = "00000002"
+        a1 = hashlib.md5("bda:Colonial Place:bda".encode()).hexdigest()
+        a2 = hashlib.md5(f"PUT:http://{self.hostport}/a5-test/limited4/foo/barbar.txt".encode()).hexdigest()
+        a2_get = hashlib.md5(f"GET:http://{self.hostport}/a5-test/limited4/foo/barbar.txt".encode()).hexdigest()
+        a2_delete = hashlib.md5(f"DELETE:http://{self.hostport}/a5-test/limited4/foo/barbar.txt".encode()).hexdigest()
+        a2_rspauth = hashlib.md5(f":http://{self.hostport}/a5-test/limited2/foo/bar.txt".encode()).hexdigest()
+        response1 = hashlib.md5(f"{a1}:{nonce}:{ncount1}:{cnonce}:auth:{a2}".encode()).hexdigest()
+        response2 = hashlib.md5(f"{a1}:{nonce}:{ncount2}:{cnonce}:auth:{a2}".encode()).hexdigest()
+        response2_get = hashlib.md5(f"{a1}:{nonce}:{ncount2}:{cnonce}:auth:{a2_get}".encode()).hexdigest()
+        rspauth2 = hashlib.md5(f"{a1}:{nonce}:{ncount2}:{cnonce}:auth:{a2_rspauth}".encode()).hexdigest()
+        response3_get = hashlib.md5(f"{a1}:{nonce}:{ncount1}:{cnonce}:auth:{a2_get}".encode()).hexdigest()
+        rspauth3 = hashlib.md5(f"{a1}:{nonce}:{ncount1}:{cnonce}:auth:{a2_rspauth}".encode()).hexdigest()
+        response4_delete = hashlib.md5(f"{a1}:{nonce}:{ncount2}:{cnonce}:auth:{a2_delete}".encode()).hexdigest()
+        rspauth4 = hashlib.md5(f"{a1}:{nonce}:{ncount2}:{cnonce}:auth:{a2_rspauth}".encode()).hexdigest()
+        return {"cnonce": cnonce, "nc1": ncount1, "nc2": ncount2, "resp1": response1, "resp2": response2, "resp2g": response2_get, "resp3g": response3_get, "resp4d": response4_delete, "rspauth2": rspauth2, "rspauth3": rspauth3, "rspauth4": rspauth4}
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_1(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_2(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_3(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_4(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_5(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_6(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_7(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_8(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_9(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_10(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_11(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_12(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_13(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_14(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_15(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
+
+
+    @HTTPTester.request("get-root.http", PATH="/a5-test/")
+    def test_16(self, report):
+        """TODO: Yet to implement!"""
+        assert False, "Yet to implement!"
