51c3783963f8ce3912d54bf4e87fa5db64ba6d72
Sawood Alam <ibnesayeed@gmail.com>
2019-09-15 21:41:04 -0400

    Download code for records

--- evaluator.sh
+++ evaluator.sh
@@ -18,24 +18,30 @@ do
     then
         continue
     fi
-    dt=`date +"%Y%m%d-%H%M%S-%Z"`
-    suitedir="$outdir/$csid/$suite"
-    mkdir -p $suitedir
-    reort="$suitedir/$csid-$suite-$dt.txt"
-    echo "Creating report: $reort"
+    dt=`date +"%Y%m%d-%H%M%S"`
+    dtz=`date +"%Z"`
+    userdir="$outdir/$csid"
+    mkdir -p $userdir
+    outfile="$userdir/$csid-$suite-$dt"
+    report="$outfile-report.txt"
+    code="$outfile-code.tar.gz"
 
-    echo "================================================================================" > $reort
-    echo "Assignment: $suite" >> $reort
-    echo "Student: $name <$csid@cs.odu.edu>" >> $reort
-    echo "Time: $dt" >> $reort
-    echo "Repository: https://github.com/$ghid/$repo/tree/$tag" >> $reort
-    echo "Server: cs531-$csid" >> $reort
-    echo "================================================================================" >> $reort
+    echo "Downloading code: $code"
+    curl -sL -o $code "https://$GITHUBKEY@github.com/$ghid/$repo/archive/$tag.tar.gz"
 
-    echo "" >> $reort
-    echo "Deploying server: cs531-$csid" | tee -a $reort
-    echo "" >> $reort
-    curl -is "http://cs531.cs.odu.edu/servers/deploy/$csid/$tag" >> $reort
+    echo "Creating report: $report"
+    echo "================================================================================" > $report
+    echo "Assignment: $suite" >> $report
+    echo "Student: $name <$csid@cs.odu.edu>" >> $report
+    echo "Time: $dt $dtz" >> $report
+    echo "Repository: https://github.com/$ghid/$repo/tree/$tag" >> $report
+    echo "Server: cs531-$csid" >> $report
+    echo "================================================================================" >> $report
+
+    echo "" >> $report
+    echo "Deploying server: cs531-$csid" | tee -a $report
+    echo "" >> $report
+    curl -s "http://cs531.cs.odu.edu/servers/deploy/$csid/$tag" >> $report
 
     sleep 5
 
@@ -43,17 +49,17 @@ do
 
     if [[ 0 -eq $? ]]
     then
-        echo "" >> $reort
-        echo "Testing server: cs531-$csid against $suite test suite" | tee -a $reort
-        echo "" >> $reort
-        ./main.py "cs531-$csid" $suite >> $reort
+        echo "" >> $report
+        echo "Testing server: cs531-$csid against $suite test suite" | tee -a $report
+        echo "" >> $report
+        ./main.py "cs531-$csid" $suite >> $report
 
-        echo "" >> $reort
-        echo "Destroying server: cs531-$csid" | tee -a $reort
-        curl -is "http://cs531.cs.odu.edu/servers/destroy/$csid" >> $reort
+        echo "" >> $report
+        echo "Destroying server: cs531-$csid" | tee -a $report
+        curl -s "http://cs531.cs.odu.edu/servers/destroy/$csid" >> $report
     fi
 
-    echo "" >> $reort
+    echo "" >> $report
 done
 
 echo "All done!"
